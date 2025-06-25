a8169e49aacb4da15d059ddd58da3bd283957fcc
Piyush Kumar Jayswal <pyushjaiswal369@gmail.com>
2025-06-25 14:57:01 +0530

    Update FatherSonApplicationTests.java

--- src/test/java/com/example/demo/FatherSonApplicationTests.java
+++ src/test/java/com/example/demo/FatherSonApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FatherSonApplicationTests {
+public class FatherSonApplicationTests {
 
 	@Test
 	void contextLoads() {
